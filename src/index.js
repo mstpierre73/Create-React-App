444a04b3967c954d08b1a0e8be1852aa6e5547fe
mstpierre73 <naranjaqc@hotmail.com>
2019-05-28 12:08:33 -0400

    "add files from eating my_first_app"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import fruits from "./foods";
+import {choice, remove} from "./helpers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log("I am hungry. I would like some fruits.");
+console.log("We have " + fruits.length + " options.");
+let myChoice = choice(fruits);
+console.log("I'd like one " + myChoice + ", please.");
+console.log("Here you go: " + myChoice);
+console.log("Delicious! May I have another ?");
+remove(fruits, myChoice);
+console.log("I'm sorry, we're all out. We have " + fruits.length + " left.");
+console.log(fruits);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
