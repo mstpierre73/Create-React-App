62777df72846a37b58f73a94b293c09d45d10d52
mstpierre73 <naranjaqc@hotmail.com>
2019-05-28 16:05:58 -0400

    "messed up files by trying to merge two projects"

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Dog from "./Dog";
+
+class App extends Component {
+    render(){
+        return(
+            <div className = "App">
+                <Dog />
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import React, { Component } from 'react'
 import fruits from "./foods";
 import {choice, remove} from "./helpers";
+import App from "./App";
 
 console.log("I am hungry. I would like some fruits.");
 console.log("We have " + fruits.length + " options.");
@@ -10,4 +12,18 @@ console.log("Delicious! May I have another ?");
 remove(fruits, myChoice);
 console.log("I'm sorry, we're all out. We have " + fruits.length + " others fruits left.");
 console.log(fruits);
+
+class Index extends Component{
+    render(){
+        return(
+            <div>
+                <App/>
+            </div>
+        );
+    }
+}
+
+export default Index
+
+
 
